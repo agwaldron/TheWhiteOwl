de2f492c29fafac3abc270018617c54f5111be2a
Alec Waldron <awal_dron@yahoo.com>
2017-12-14 04:31:38 -0700

    Main activity design

--- App/app/src/main/java/com/example/alec/thewhiteowl/MainActivity.java
+++ App/app/src/main/java/com/example/alec/thewhiteowl/MainActivity.java
@@ -5,20 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+    TextView nameText;
+    TextView announcementText;
     Button dailySpecialsButton;
     Button foodMenuButton;
     Button beerMenuButton;
     Button directionsButton;
+    TextView contactText;
+    TextView visitText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        nameText = (TextView)findViewById(R.id.name_text);
+        nameText.setTextColor(getResources().getColor(R.color.colorLight, null));
+
+        announcementText = (TextView)findViewById(R.id.announcement_text);
+        announcementText.setTextColor(getResources().getColor(R.color.colorAnnouncement, null));
+
         dailySpecialsButton = (Button)findViewById(R.id.daily_specials_button);
+        dailySpecialsButton.setTextColor(getResources().getColor(R.color.colorDark, null));
         dailySpecialsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         foodMenuButton = (Button)findViewById(R.id.food_menu_button);
+        foodMenuButton.setTextColor(getResources().getColor(R.color.colorDark, null));
         foodMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         beerMenuButton = (Button)findViewById(R.id.beer_menu_button);
+        beerMenuButton.setTextColor(getResources().getColor(R.color.colorDark, null));
         beerMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,12 +59,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
         directionsButton = (Button)findViewById(R.id.directions_button);
+        directionsButton.setTextColor(getResources().getColor(R.color.colorDark, null));
         directionsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 redirectDirections();
             }
         });
+
+        contactText = (TextView)findViewById(R.id.contact_text);
+        contactText.setTextColor(getResources().getColor(R.color.colorLight, null));
+
+        visitText = (TextView)findViewById(R.id.visit_text);
+        visitText.setTextColor(getResources().getColor(R.color.colorLight, null));
     }
 
     public void redirectDailySpecials(){
